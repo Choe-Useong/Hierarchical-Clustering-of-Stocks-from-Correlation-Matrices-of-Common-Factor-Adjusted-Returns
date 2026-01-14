0d5e2dba0183f133cb858cc5e44162ff968aa353
Choe-Useong <cwsu0313@naver.com>
2026-01-14 14:35:49 +0900

    Update 1_parquet_chunks.py

--- 1_parquet_chunks.py
+++ 1_parquet_chunks.py
@@ -21,8 +21,7 @@ os.makedirs(OUT_DIR, exist_ok=True)
 for item, g in data2.groupby(ITEM_COL, sort=False):
     w = g.set_index(["Symbol", "Symbol Name"])[date_cols]
 
-    # 빈칸/병합셀 보정
-    w = w.ffill(axis=1).infer_objects(copy=False)
+    w = w.infer_objects(copy=False)
 
     safe = re.sub(r"[\\/:*?\"<>|]", "_", str(item))
     w.to_parquet(os.path.join(OUT_DIR, f"item__{safe}.parquet"), engine="pyarrow")
@@ -32,4 +31,5 @@ path = r"items_parquet/item__수정주가(원).parquet"
 
 w = pd.read_parquet(path)
 print(w.iloc[:5, :5].to_string())
+
 
